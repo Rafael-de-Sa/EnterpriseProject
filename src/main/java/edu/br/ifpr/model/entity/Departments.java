2e7f5ca577aac0c6080cf9548ce9cf178544d97f
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-06-08 12:48:09 -0300

    :sparkles: feat: Department mapping

---
+++ src/main/java/edu/br/ifpr/model/entity/Departments.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.br.ifpr.model.entity;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author rafael
+ */
+@Entity
+public class Departments {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 50, nullable = false)
+    @Basic
+    private String name;
+
+    @Column(precision = 12, scale = 2, nullable = false)
+    @Basic
+    private BigDecimal budget;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getBudget() {
+        return budget;
+    }
+
+    public void setBudget(BigDecimal budget) {
+        this.budget = budget;
+    }
+
+}
